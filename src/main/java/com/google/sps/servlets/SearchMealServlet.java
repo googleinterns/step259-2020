5de1d5ed80c107290fe467d55793a7ab28c4224f
sandatsian <polinasand@google.com>
2020-08-13 18:03:39 +0000

    Add Apache head

--- src/main/java/com/google/sps/servlets/SearchMealServlet.java
+++ src/main/java/com/google/sps/servlets/SearchMealServlet.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps.servlets;
 
 import java.io.IOException;
@@ -42,15 +56,14 @@ public class SearchMealServlet extends HttpServlet {
         response.sendRedirect("/index.html");
     }
     
-    private ArrayList<String> searchDishByKeyWord(String request) {
+    private ArrayList<String> searchDishByKeyWord(String requestedDish) {
         ArrayList<String> result = new ArrayList<String>();
 
         for (String dish : this.dishes) {
-            if (dish.contains(request)) {
+            if (dish.contains(requestedDish)) {
                 result.add(dish);
             }
         }
-        
         return result;
     }
 
