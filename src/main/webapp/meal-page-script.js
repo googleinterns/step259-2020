48d3aa1da4c79bc0e48f54e21fdb93508ab8e995
Kozlova Olga <grenlayk@google.com>
2020-08-18 11:53:15 +0000

    Add restaurants and locate user

--- src/main/webapp/meal-page-script.js
+++ src/main/webapp/meal-page-script.js
@@ -13,24 +13,27 @@
 // limitations under the License.
 
 function fetchMealInfo() {
-    fetch('/meal').then(response => response.json()).then((meal) => 
-    {
-        const { title, description, ingredients } = meal;
-        const titleElement = document.getElementById("title");
-        titleElement.innerText = encodingCheck(title);
-        const descriptionElement = document.getElementById("description");
-        descriptionElement.innerText = encodingCheck(description);
-        const ingredientsElement = document.getElementById("ingredients");
-        for (const ingredient of ingredients) {
-            ingredientsElement.appendChild(createElementByTag(encodingCheck(ingredient), 'li'));
-        }
-        createMap();
+  fetch("/meal/0")
+    .then((response) => response.json())
+    .then((meal) => {
+      const { title, description, ingredients } = meal;
+      const titleElement = document.getElementById("title");
+      titleElement.innerText = encodingCheck(title);
+      const descriptionElement = document.getElementById("description");
+      descriptionElement.innerText = encodingCheck(description);
+      const ingredientsElement = document.getElementById("ingredients");
+      for (const ingredient of ingredients) {
+        ingredientsElement.appendChild(
+          createElementByTag(encodingCheck(ingredient), "li")
+        );
+      }
+      createMap();
     });
 }
 
 // solution from https://stackoverflow.com/questions/20174280/nodejs-convert-string-into-utf-8
 function encodingCheck(string) {
-    return JSON.parse(JSON.stringify(string));
+  return JSON.parse(JSON.stringify(string));
 }
 
 function createElementByTag(text, tag) {
@@ -39,21 +42,79 @@ function createElementByTag(text, tag) {
   return element;
 }
 
-/** Creates a map and adds it to the page. */
+let map;
+
 function createMap() {
-  const userLocation = new google.maps.LatLng(55.746514, 37.627022);
+  let userLocation = new google.maps.LatLng(55.746514, 37.627022);
   const mapOptions = {
-    zoom: 16,
-    center: userLocation
+    zoom: 14,
+    center: userLocation,
   };
-  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  // TODO(grenlayk): implement real geolocation here
-  // TODO(grenlayk): change "You are here" marker on custom one
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
   const locationMarker = new google.maps.Marker({
     position: userLocation,
     map,
-    title: "You are here",
+    title: "You are here (probably)",
+    label: "U",
     animation: google.maps.Animation.DROP,
   });
+
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const location = new google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        );
+        map.setCenter(location);
+        locationMarker.setPosition(location);
+        addRestaurants();
+      },
+      function () {
+        handleLocationError(true);
+        addRestaurants();
+      }
+    )
+  } else {
+    handleLocationError(false);
+    addRestaurants();
+  }
+}
+
+function addRestaurants() {
+  // Hard coded restaurants
+  const restaurants = [];
+  restaurants.push(changeLocation(map.getCenter(), 0.001, 0.001));
+  restaurants.push(changeLocation(map.getCenter(), -0.001, 0.001));
+
   // TODO(grenlayk): implement restaurants search here
+  for (const restaurantLocation of restaurants) {
+    const marker = new google.maps.Marker({
+        position: restaurantLocation,
+        map,
+        title: "Restaurant",
+        animation: google.maps.Animation.DROP
+    });
+  }
+}
+
+function changeLocation(location, latDiff, lngDiff) {
+    const lat = location.lat() + latDiff;
+    const lng = location.lng() + lngDiff;
+    return {
+        lat, 
+        lng
+    };
+}
+
+function handleLocationError(browserHasGeolocation) {
+  if (browserHasGeolocation) {
+    alert(
+      "Error: The Geolocation service failed.\n \
+               Share your location, please."
+    );
+  } else {
+    alert("Error: Your browser doesn't support geolocation.");
+  }
 }
