2eeb7079f326b69cdac1f7901f8cc675577d2b36
Kozlova Olga <grenlayk@google.com>
2020-09-15 08:42:35 +0000

    Fix formatting

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -3,7 +3,12 @@ Vue.component("photo-copyright", {
     <div id="source">
       This web page uses images from 
       <a href="https://www.freepik.com/"> 
-        freepik.com</a> and data from <a href="https://en.wikibooks.org/wiki/Cookbook:Table_of_Contents">wikibooks.org</a>
+        freepik.com
+      </a> 
+      and data from 
+      <a href="https://en.wikibooks.org/wiki/Cookbook:Table_of_Contents">
+        wikibooks.org
+      </a>
     </div>
   `,
 });
